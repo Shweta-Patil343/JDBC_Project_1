7c9825feb3cd1905563e8ceff3301c227f8aeca1
Shweta-Patil343 <patilshwetapatil45@gmail.com>
2025-05-06 09:31:52 +0530

    Update Program1.java

--- src/com/test/Program1.java
+++ src/com/test/Program1.java
@@ -1,9 +1,15 @@
 package com.test;
-
+import java.util.Scanner;
 public class Program1 {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hello world!");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the num");
+		int num=sc.nextInt();
+		res=num*num;
+		System.out.println("res"+res);
 	}
 
 }
